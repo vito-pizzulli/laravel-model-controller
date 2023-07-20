a88208b2b4f3efcf601f6c55dece3106cecfc2f9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-20 15:17:14 +0200

    added index function to PageController that returns the home view

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PageController extends Controller
-{
-    //
+class PageController extends Controller {
+    public function index() {
+        return view('guest.home');
+    }
 }
