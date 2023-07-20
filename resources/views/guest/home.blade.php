3049a64aa4e5207ace2dbd0c93d684bfc128ab48
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-20 16:32:28 +0200

    added foreach to print every movie from the movies table passed by controller from mysql db, added bootstrap card

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.app')
 
-@section('title', 'Homepage')
+@section('title', 'Laravel Model Controller')
 
 @section('main-section')
-    <h1>Welcome to the Homepage!</h1>
+    <div class="card-container d-flex flex-wrap">
+        @foreach($movies as $movie)
+            <div class="card m-3" style="width: calc(100% / 5 - 2rem);">
+                <div class="card-body">
+                    <h2>Title: {{ $movie['title'] }}</h2>
+                    <h3>Original title: {{ $movie['original_title'] }}</h3>
+                    <p>Nationality: {{ $movie['nationality'] }}</p>
+                    <p>Release date: {{ $movie['date'] }}</p>
+                    <p>Vote: {{ $movie['vote'] }}</p>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
